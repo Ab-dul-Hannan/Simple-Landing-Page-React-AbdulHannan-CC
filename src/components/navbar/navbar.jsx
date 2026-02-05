377947d6af8ff0ecf614d1ff282e60ad853cc356
Abdul Hannan <abdunknownsamurai@gmail.com>
2026-02-05 21:59:36 +0500

    styling

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -5,7 +5,7 @@ import { CornerDownRight } from 'lucide-react'
 const Navbar = () => {
   return (
     <div className={styles.navbar}>
-      <div className={styles.logobtn}>Landing Page</div>
+      <div className={styles.logobtn}><span>Landing Page</span></div>
       <div className={styles.announce}>
         <CornerDownRight size={18} color="#1c2120" /> <p>2026 &copy; <span>Abdul Hannan.</span> All Rights Reserved</p>
       </div>
